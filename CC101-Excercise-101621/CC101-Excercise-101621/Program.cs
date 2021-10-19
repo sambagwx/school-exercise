e2cb7b158756723f9958ff236546a78e218af86f
Eric Tierra <sambag.wx@gmail.com>
2021-10-19 23:37:36 +0800

    Update Program.cs

--- CC101-Excercise-101621/CC101-Excercise-101621/Program.cs
+++ CC101-Excercise-101621/CC101-Excercise-101621/Program.cs
@@ -9,13 +9,13 @@ namespace CC101_Excercise_101621
             //CC101 Exercise | October 16, 2021
         {
             Console.WriteLine("************************************************");
-            Console.WriteLine();
-            Console.WriteLine("*   I am Eric S. Tierra                        *");
-            Console.WriteLine("*   I live in Atimonan, Quezon                 *");
-            Console.WriteLine("*   I was born on Dec. 29, 1990                *");
-            Console.WriteLine("*   I am taking BSCS at QECI                   *");
+            Console.WriteLine("*                                              *");
+            Console.WriteLine("*           I am Eric S. Tierra                *");
+            Console.WriteLine("*       I live in Atimonan, Quezon             *");
+            Console.WriteLine("*       I was born on Dec. 29, 1990            *");
+            Console.WriteLine("*        I am taking BSCS at QECI              *");
             Console.WriteLine("*   I want to be a computer programmer someday *");
-            Console.WriteLine();
+            Console.WriteLine("*                                              *");
             Console.WriteLine("************************************************");
         }
     }
