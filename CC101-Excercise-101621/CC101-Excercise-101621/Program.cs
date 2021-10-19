ff91c65e6dfa690877f3c2084e8765194ddd2bf0
Eric Tierra <91515255+sambagwx@users.noreply.github.com>
2021-10-19 23:34:34 +0800

    Add files via upload

---
+++ CC101-Excercise-101621/CC101-Excercise-101621/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CC101_Excercise_101621
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+            //CC101 Exercise | October 16, 2021
+        {
+            Console.WriteLine("************************************************");
+            Console.WriteLine();
+            Console.WriteLine("*   I am Eric S. Tierra                        *");
+            Console.WriteLine("*   I live in Atimonan, Quezon                 *");
+            Console.WriteLine("*   I was born on Dec. 29, 1990                *");
+            Console.WriteLine("*   I am taking BSCS at QECI                   *");
+            Console.WriteLine("*   I want to be a computer programmer someday *");
+            Console.WriteLine();
+            Console.WriteLine("************************************************");
+        }
+    }
+}
